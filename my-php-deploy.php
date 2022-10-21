fe8e52c7b5df58be374b79aef0b3f6339abedcb2
QGeekDev <olentsyk.qualitygeek@gmail.com>
2022-10-21 15:38:14 +0300

    test

--- my-php-deploy.php
+++ my-php-deploy.php
@@ -1,9 +1,9 @@
 <?php
 # webhook for project deployment
 if ($_GET['token'] === 'secret_key') {
-$cmd = shell_exec("rm -rf my-php-deploy
+$cmd = shell_exec("rm -rf github-cpanel-sample
 && git clone https://developerToken@github.com/QGeekDev/github-cpanel-sample.git
-&& rm -rf my-php-deploy/.git
+&& rm -rf github-cpanel-sample/.git
 );
 echo $cmd;
 echo 'Deployment sucess';
