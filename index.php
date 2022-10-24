a5249f331f719608d48f658803db074a99fba55a
QGeekDev <olentsyk.qualitygeek@gmail.com>
2022-10-24 13:01:55 +0300

    test

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Document</title>
 </head>
 <body>
- <?php echo "GOGA, this time it's okay test developerToken test webhook Good111";?>
+ <?php echo "GOGA, this time it's okay test developerToken test webhook Good";?>
     
 </body>
 </html>
